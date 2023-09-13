eced20f3f7f55e8de841802fe4260244f6695560
Jake Cannon <svenskakid12@gmail.com>
2023-09-13 13:47:14 -0600

    Preparing the program for SQL connections

---
+++ SAFE PMA Members/Member.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAFE_PMA_Members
+{
+    public class Member
+    {
+
+    }
+}
